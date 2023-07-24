ea04b836148a4132605df988c4a1c94dbc9c8bdb
BrunoTeixeira1996 <brunoalexandre3@hotmail.com>
2023-07-24 14:06:16 +0100

    delted useless scripts

--- backup_scripts/postgresql_backup.sh
+++
@@ -1,4 +0,0 @@
-ssh bot 'pg_dump waiw > waiw.sql && pg_dump leaks > leaks.sql'
-rsync -Pav -e "ssh -i $HOME/.ssh/id_ed25519_postgresql_backup" bot:/root/*.sql /mnt/pve/external/postgresql_backup # postgres -> external hard drive
-cp  /mnt/pve/external/postgresql_backup/*.sql /storagepool/backups/postgresql_backup # external hard drive -> storage pool (hdd in proxmox)
-ssh bot 'rm waiw.sql && rm leaks.sql'
--- backup_scripts/syncthing_backup.sh
+++
@@ -1,2 +0,0 @@
-rsync -Pav -e "ssh -i $HOME/.ssh/id_ed25519_syncthing_backup" syncthing:/root/config/Sync /mnt/pve/external/syncthing_backup # syncthing lxc -> external hard drive
-cp -r /mnt/pve/external/syncthing_backup/Sync /storagepool/backups/syncthing_backup # external hard drive to storage pool (hdd in proxmox)
