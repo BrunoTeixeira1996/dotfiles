d1963d2a25a573b6d00d705228a7a2232b8fdb8b
BrunoTeixeira1996 <brunoalexandre3@hotmail.com>
2025-02-22 17:39:10 +0000

    added double monitor script

---
+++ stow_folder/screenlayout/.screenlayout/work_double_monitor.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --off --output DP-1 --primary --mode 1920x1080 --rate 144 --pos 1920x0 --rotate normal --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --mode 1920x1080 --rate 100 --pos 0x0 --rotate normal
